dabd03e0b1fd2d3b3cb5ff04f158aa69e4682822
Alexander Greco <33609792+Alexander5015@users.noreply.github.com>
2019-09-08 22:08:29 -0600

    Commenting in robot map

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -1,15 +1,21 @@
 package frc.robot;
 
+//import motor controller libraries
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+//import Drivetrain and SpeedController Groups
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+//import camera libraries
 import edu.wpi.cscore.UsbCamera;
 import edu.wpi.first.cameraserver.CameraServer;
+//import solenoids
 import edu.wpi.first.wpilibj.DoubleSolenoid;
-import edu.wpi.first.wpilibj.SpeedControllerGroup;
+//import shuffleboard
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 
 public class RobotMap {
+    //Motor Controllers
     private CANSparkMax frontLeftMotor = new CANSparkMax(0, MotorType.kBrushless);
     private CANSparkMax rearLeftMotor = new CANSparkMax(1, MotorType.kBrushless);
     private CANSparkMax frontRightMotor = new CANSparkMax(2, MotorType.kBrushless);
@@ -19,22 +25,26 @@ public class RobotMap {
     private SpeedControllerGroup rightMotors = new SpeedControllerGroup(rearRightMotor, frontRightMotor);
     private SpeedControllerGroup leftMotors = new SpeedControllerGroup(rearLeftMotor, frontLeftMotor);
 
-    //Add drivetrain
+    //Add Drivetrain
     private DifferentialDrive driveTrain = new DifferentialDrive(leftMotors, rightMotors);
     
-    //gear shifting Solenoid
+    //Gear Shifting Solenoid
     private DoubleSolenoid gearShiftSolenoid = new DoubleSolenoid(0, 1);
 
     public RobotMap() {
+        //Set camera variables and Add to Shuffleboard
         UsbCamera camera = CameraServer.getInstance().startAutomaticCapture(0);
         Shuffleboard.getTab("Camera").add(camera);
         UsbCamera camera2 = CameraServer.getInstance().startAutomaticCapture(1);
         Shuffleboard.getTab("Camera").add(camera2);
         camera.setFPS(30);
         camera2.setFPS(30);
+        //sync motors
         rearLeftMotor.follow(frontLeftMotor);
         rearRightMotor.follow(frontRightMotor);
     }
+
+    //Get Motors
     public SpeedControllerGroup getLeftMotors() {
         return leftMotors;
     }
@@ -42,11 +52,11 @@ public class RobotMap {
     public SpeedControllerGroup getRightMotors() {
         return rightMotors;
     }
-
+    //Get Gear Shift
     public DoubleSolenoid getGearShift() {
         return gearShiftSolenoid;
     }
-
+    //Get Drivetrain
     public DifferentialDrive getDriveTrain() {
         return driveTrain;
     }
