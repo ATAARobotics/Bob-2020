72caa66bc05231e95be2e386fa1c43163aca5965
BEN1JEN <benpheard@gmail.com>
2020-01-11 12:55:36 -0700

    Add colour guessing code.

--- src/main/java/frc/robot/Teleop.java
+++ src/main/java/frc/robot/Teleop.java
@@ -1,5 +1,7 @@
 package frc.robot;
 
+import java.lang.Math;
+
 import com.revrobotics.ColorSensorV3;
 
 import java.io.FileWriter;
@@ -13,7 +15,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 public class Teleop {
     // Vairables for robot classes
     private OI joysticks = null;
-    
+
     private ColorMatch colorMatcher = null;
     private ColorSensorV3 colorSensor = null;
 
@@ -47,12 +49,50 @@ public class Teleop {
 
         colorData += Integer.toString(colorSensor.getRed()) + "," + Integer.toString(colorSensor.getGreen()) + "," + Integer.toString(colorSensor.getBlue()) + "," + Integer.toString(colorSensor.getProximity()) + "\n";
 
-        
+        // Get normalized colours (0-1 where 1 means equal to the brightest colour)
+        double normRed = colorSensor.getRed();
+        double normGreen = colorSensor.getGreen();
+        double normBlue = colorSensor.getBlue();
 
-        SmartDashboard.putNumber("Red", colorSensor.getRed());
-        SmartDashboard.putNumber("Green", colorSensor.getGreen());
-        SmartDashboard.putNumber("Blue", colorSensor.getBlue());
+        double max = Math.max(Math.max(normRed, normGreen), normBlue);
+
+        normRed /= max;
+        normGreen /= max;
+        normBlue /= max;
+
+        // Give raw colour values to smart dashboard
+        SmartDashboard.putNumber("Red", normRed);
+        SmartDashboard.putNumber("Green", normGreen);
+        SmartDashboard.putNumber("Blue", normBlue);
         SmartDashboard.putNumber("Proximity", colorSensor.getProximity());
+
+        // Compute the difference between the actual colour and the expected colour for each colourwheel colour
+        double redPercent = Math.abs(normRed-1.0) + Math.abs(normGreen-0.8) + Math.abs(normBlue-0.2);
+        double yellowPercent = Math.abs(normGreen-1.0) + Math.abs(normRed-0.5) + Math.abs(normBlue-0.2);
+        double greenPercent = Math.abs(normGreen-1.0) + Math.abs(normBlue-0.3) + Math.abs(normRed-0.2);
+        double bluePercent = Math.abs(normBlue-1.0) + Math.abs(normGreen-1.0) + Math.abs(normRed-0.2);
+
+        // Give the differences for each of the four colourwheel colours to smart dashboard
+        SmartDashboard.putNumber("Red", redPercent);
+        SmartDashboard.putNumber("Yellow", yellowPercent);
+        SmartDashboard.putNumber("Green", greenPercent);
+        SmartDashboard.putNumber("Blue", bluePercent);
+
+        // Compute a string containing the name of the closest matched colour
+        double highest = Math.max(Math.max(redPercent, yellowPercent), Math.max(greenPercent, bluePercent));
+
+        String guess = "";
+        if (redPercent == highest) {
+            guess = "red";
+        } else if (yellowPercent == highest) {
+            guess = "yellow";
+        } else if (greenPercent == highest) {
+            guess = "green";
+        } else if (bluePercent == highest) {
+            guess = "blue";
+        }
+
+        SmartDashboard.putString("Guess", guess);
 
         joysticks.checkInputs();
     }
