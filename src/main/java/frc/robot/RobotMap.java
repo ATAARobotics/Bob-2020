787348313fb7392eac0d07328cb49c0c5a8e387e
BEN1JEN <benpheard@gmail.com>
2020-01-11 12:33:47 -0700

    Took color readings from control panel swatches
    Add just Jacob's colour sensor code changes

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -1,4 +1,7 @@
 package frc.robot;
+import edu.wpi.first.wpilibj.I2C;
+
+import com.revrobotics.ColorSensorV3;
 
 public class RobotMap {
 
@@ -6,5 +9,17 @@ public class RobotMap {
      * Creates a RobotMap for mapping electronics on the robot to Java Objects,
      * so that they can be used multiple places throughout the code.
      */
-    public RobotMap() {}
+
+    private final I2C.Port colorPort = I2C.Port.kOnboard;
+
+    private final ColorSensorV3 colorSensor = new ColorSensorV3(colorPort);
+
+    public RobotMap() {
+
+    }
+
+    public ColorSensorV3 getColorSensor() {
+        return colorSensor;
+    }
+
 }
--- src/main/java/frc/robot/Teleop.java
+++ src/main/java/frc/robot/Teleop.java
@@ -1,17 +1,37 @@
 package frc.robot;
 
+import com.revrobotics.ColorSensorV3;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+import com.revrobotics.ColorMatch;
+
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 public class Teleop {
     // Vairables for robot classes
     private OI joysticks = null;
-    private RobotMap robotMap = null;
+    
+    private ColorMatch colorMatcher = null;
+    private ColorSensorV3 colorSensor = null;
+
+    private FileWriter colorLog = null;
+
+    private String colorData = "";
 
     /**
      * Creates a Teleop object which is the main file when in teleop mode.
      */
-    public Teleop(RobotMap robotMap) {
+    public Teleop(ColorSensorV3 colorSensor) {
+
         // Initialize Classes
-        robotMap = new RobotMap();
         joysticks = new OI();
+
+        this.colorSensor = colorSensor;
+        colorMatcher = new ColorMatch();
+
     }
 
     /**
@@ -24,6 +44,16 @@ public class Teleop {
      * Function that's called in an infinite loop on the robot when in teleop mode.
      */
     public void TeleopPeriodic() {
+
+        colorData += Integer.toString(colorSensor.getRed()) + "," + Integer.toString(colorSensor.getGreen()) + "," + Integer.toString(colorSensor.getBlue()) + "," + Integer.toString(colorSensor.getProximity()) + "\n";
+
+        
+
+        SmartDashboard.putNumber("Red", colorSensor.getRed());
+        SmartDashboard.putNumber("Green", colorSensor.getGreen());
+        SmartDashboard.putNumber("Blue", colorSensor.getBlue());
+        SmartDashboard.putNumber("Proximity", colorSensor.getProximity());
+
         joysticks.checkInputs();
     }
 
@@ -32,5 +62,27 @@ public class Teleop {
      */
 	public void TestPeriodic() {
         joysticks.checkInputs();
-	}
+    }
+    public void disabledInit() {
+        try {
+            colorLog = new FileWriter("home/lvuser/colorLog.csv");
+        } catch (IOException e) {
+            return;
+        }
+
+        try {
+            colorLog.write(colorData);
+            colorLog.flush();
+        } catch (IOException e) {
+            System.out.println(e.toString());
+            return;
+        }
+
+        try {
+            System.out.println("Closing");
+            colorLog.close();
+        } catch (Exception e) {
+            return;
+        }
+    }
 }
